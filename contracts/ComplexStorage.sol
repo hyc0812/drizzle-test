db415b85cf647762539fabdde43cc6beb9c7272c
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-05-14 16:49:58 -0600

    update version

--- contracts/ComplexStorage.sol
+++ contracts/ComplexStorage.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.5.16;
+pragma solidity >=0.4.22 <0.9.0;
 
 contract ComplexStorage {
     uint public storeduint1 = 15;
--- contracts/TutorialToken.sol
+++ contracts/TutorialToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.5.16;
+pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.0/contracts/token/ERC20/ERC20.sol";

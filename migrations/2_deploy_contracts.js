06a827db21cd98ee3a0df4f4238d9b938999c500
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-05-14 16:15:30 -0600

    Add EtherTransfer.sol

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,10 +2,13 @@ const SimpleStorage = artifacts.require("SimpleStorage");
 const TutorialToken = artifacts.require("TutorialToken");
 const ComplexStorage = artifacts.require("ComplexStorage");
 const CryptoKids = artifacts.require("CryptoKids");
+const EtherTransfer = artifacts.require("EtherTransfer");
+
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(TutorialToken);
   deployer.deploy(ComplexStorage);
   deployer.deploy(CryptoKids);
+  deployer.deploy(EtherTransfer);
 };
